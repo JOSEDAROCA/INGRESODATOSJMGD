a38b6c71e17b4ed25370e3af5bb7637e9e785730
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-09-15 16:30:08 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  let datoNumerico: number = Number(dato.value);
+  console.log("el dato ingresado es ", datoNumerico);
+  //}
+});
